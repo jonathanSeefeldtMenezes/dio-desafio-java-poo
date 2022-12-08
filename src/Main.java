821211eebba119bcbe737bd7d919c6baf01f17f7
Jonathan Menezes <sitioinga@gmail.com>
2022-12-07 20:31:34 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,32 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
-        curso1.setCargaHoraria(8);
+        
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("curso Java");
+        cursoJava.setDescricao("descrição curso java");
+        cursoJava.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
-        curso2.setCargaHoraria(4);
+        Curso cursoJavaScript = new Curso();
+        cursoJavaScript.setTitulo("curso js");
+        cursoJavaScript.setDescricao("descrição curso js");
+        cursoJavaScript.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
-        mentoria.setData(LocalDate.now());
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("mentoria de java");
+        mentoriaJava.setDescricao("descrição mentoria java");
+        mentoriaJava.setData(LocalDate.now());
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
-
-        Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
-        bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
-        bootcamp.getConteudos().add(mentoria);
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("Bootcamp Java Developer");
+        bootcampJava.setDescricao("Descrição Bootcamp Java Developer");
+        bootcampJava.addConteudo(cursoJava);
+        bootcampJava.addConteudo(cursoJavaScript);
+        bootcampJava.addConteudo(mentoriaJava);
 
         Dev devCamila = new Dev();
         devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
+        devCamila.inscreverBootcamp(bootcampJava);
         System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
         devCamila.progredir();
         devCamila.progredir();
@@ -48,7 +45,7 @@ public class Main {
 
         Dev devJoao = new Dev();
         devJoao.setNome("Joao");
-        devJoao.inscreverBootcamp(bootcamp);
+        devJoao.inscreverBootcamp(bootcampJava);
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         devJoao.progredir();
         devJoao.progredir();
--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -7,13 +7,13 @@ import java.util.Objects;
 import java.util.Set;
 
 public class Bootcamp {
+
     private String nome;
     private String descricao;
     private final LocalDate dataInicial = LocalDate.now();
     private final LocalDate dataFinal = dataInicial.plusDays(45);
     private Set<Dev> devsInscritos = new HashSet<>();
     private Set<Conteudo> conteudos = new LinkedHashSet<>();
-
 
     public String getNome() {
         return nome;
@@ -53,6 +53,10 @@ public class Bootcamp {
 
     public void setConteudos(Set<Conteudo> conteudos) {
         this.conteudos = conteudos;
+    }
+
+    public void addConteudo(Conteudo conteudo){
+        this.conteudos.add(conteudo);
     }
 
     @Override
